30f0dde1d7a8e3ffba1de1be8bd1c2028d6d98f3
Rishi00009 <rishirishim0@gmail.com>
2024-06-16 13:14:30 +0530

    Add new tas2

---
+++ Day 3/Anonymous function & IIFE.js
@@ -0,0 +1,72 @@
+// a. Print odd numbers in an arrey ( anonymous function )
+var output = function(input) {
+        arr  =  input
+        var l = arr.length
+        var arr2 = []
+        for(var i = 0 ; i< l ; i++){
+            if(Number(arr[i])%2!=0){
+                arr2.push(arr[i])
+            }
+        }
+        
+        console.log(arr2)
+}
+output(["1", "2", "3", "4", "5"])
+
+// a. Print add numbers in an arrey ( IIFE )
+
+(function(input) {
+    arr  =  input
+    var l = arr.length
+    var arr2 = []
+    for(var i = 0 ; i< l ; i++){
+        if(Number(arr[i])%2!=0){
+            arr2.push(arr[i])
+        }
+    }
+    
+    console.log(arr2)
+})(["1" , "2" , "4" , "6" , "7"]);
+
+// c. Sum of all numbers in an arrey  ( anonymous function )
+
+var input = function(arr) {
+        var a = arr
+        var l = arr.length
+        var output = 0
+        for(var i = 0 ; i < l ; i++){
+            output = output + Number(arr[i])
+        }
+        console.log(output)
+}
+
+input(["5" , " 10" , "15" , "20"])
+
+// c. sum of all numbers in an arrey ( IIFE )
+
+(function(input){
+    var arr = input
+    var l = arr.length
+    var output = 0
+    for(var i = 0 ; i < l ; i++){
+        output = output + Number(arr[i])
+    }
+    console.log(output)
+})(["10" , "20" , "2" , "25"])
+
+// d. return all the prime numbers in arrey ( anonymous function )
+
+var primenumbers = function(numbers){
+    var arr = numbers
+    var l = arr.length
+    var arr2 = []
+    for(var i = 0 ; i < l ; i++){
+        if(Number(numbers[i])%2!=0 && Number(numbers[i])%3!=0){
+            arr2.push(numbers[i])
+        }
+    }
+    var output = arr2.join(" ")
+    console.log(output)
+}
+
+primenumbers(["5" , "7" , "9" , "12" , "13"])
