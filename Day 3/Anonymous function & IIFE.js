652cee8bd888359a80a3bec56b822d4e4c6d389f
Rishi00009 <rishirishim0@gmail.com>
2024-06-18 21:12:04 +0530

    Working

--- Day 3/Anonymous function & IIFE.js
+++ Day 3/Anonymous function & IIFE.js
@@ -118,3 +118,99 @@ input(["MalayalaM" , "Rishi"])
     }
 })(["malayalam" , "english"])
 
+// f . Return median of two sorted arreys of the same size ( anonymous funcion )
+
+var input = function(a){
+    var arr1 = a[0]
+    var arr2 = a[1]
+    var arr  = [] 
+    for(var i = 0 ; i < arr1.length ; i++){
+        arr.push(arr1[i])
+        arr.push(arr2[i])
+    }
+    arr.sort(function(i,j) {return(i-j)})
+    var l = arr.length
+    if(l%2!=0){
+        console.log(Number(arr[(l-1)/2]))
+    }
+    else(
+        console.log((Number(arr[l/2]) + Number(arr[(l/2)-1]))/2)
+    )
+}
+
+input(  [[ 5 , 4 , 3 , 7], [6 , 8 , 3 , 9]])
+
+
+// f . Return median of two sorted arreys of the same size ( IIFE )
+
+(function(a){
+    var arr1 = a[0]
+    var arr2 = a[1]
+    var arr  = [] 
+    for(var i = 0 ; i < arr1.length ; i++){
+        arr.push(arr1[i])
+        arr.push(arr2[i])
+    }
+    arr.sort(function(i,j) {return(i-j)})
+    var l = arr.length
+    if(l%2!=0){
+        console.log(Number(arr[(l-1)/2]))
+    }
+    else(
+        console.log((Number(arr[l/2]) + Number(arr[(l/2)-1]))/2)
+    )
+})(  [[ 6 , 4 , 2 , 10], [0 , 8 , 14 , 12]])
+
+
+// g . remove duplicates from an arrey ( anonymous function )
+
+var input = function(arr){
+    let l = arr.length
+    let arr1 = []
+    for(let i = 0 ; i< l ; i++){
+        if(arr1.indexOf(arr[i])===-1  )
+            arr1.push(arr[i])
+    }
+    console.log(arr1)
+}
+
+input([1 , 2 , 1 , 5 , 4 , 2])
+
+
+// g . remove duplicates from an arrey ( IIFE )
+
+(function(arr){
+    let l = arr.length
+    let arr1 = []
+    for(let i = 0 ; i< l ; i++){
+        if(arr1.indexOf(arr[i])===-1  )
+            arr1.push(arr[i])
+    }
+    console.log(arr1)
+})([1 , 2 , 1 , 5 , 4 , 2])
+
+
+// h . Rotate an arrey by k times ( anonymous function )
+
+let rotateArray1 = function(nums, k) {
+
+    for (let i = 0; i < k; i++) {
+        nums.unshift(nums.pop());
+    }
+  
+    console.log(nums)
+  }
+rotateArray1([1,2,3,4,5] , 2)
+
+
+// h . Rotate an arrey by k times ( IIFE )
+
+
+(function(nums, k) {
+
+    for (let i = 0; i < k; i++) {
+        nums.unshift(nums.pop());
+    }
+  
+    console.log(nums)
+  })([1,2,3,4,5] , 2)
