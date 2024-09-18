c39d8525c3513cb62822c4d9329009d97c91167d
Rishi00009 <rishirishim0@gmail.com>
2024-09-18 09:28:58 +0530

    add Pricecart

--- React/PriceCart/src/App.jsx
+++ React/PriceCart/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import priceCard from './Components/Pricecard'
+import priceCard from "./Components/Pricecart"
 
 
 function App() {
   return (
     <>
      
-     
+      
       <div>{priceCard("FREE",0)}</div>
       <div>{priceCard("PLUS",9)}</div>
       <div>{priceCard("PRO",49)}</div>
